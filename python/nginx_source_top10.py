c503bd3cf3ed4749cd890f2f7c22c272ce789f6b
wjxiaoit <wjxiaoit@163.com>
2018-03-13 11:09:42 +0800

    Add nginx source top 10.

---
+++ python/nginx_source_top10.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: UTF-8 -*-
+
+from __future__ import print_function
+from collections import Counter
+
+c = Counter()
+with open('/root/xm/access.log') as f:
+    for line in f:
+        c[line.split()[6]] += 1
+
+print ("Popular resources : {0}".format(c.most_common(10)))
+
